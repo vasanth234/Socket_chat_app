79d3f8ac6a3d1c3afa14f1ec06e0c7b6da9e673a
vasanth234 <pipparivasanth66700@gmail.com>
2024-12-25 14:40:38 +0530

    changed again

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const dotenv = require("dotenv");
-const winston = require("winston");
 
 // Load environment variables
 dotenv.config();
@@ -22,21 +21,6 @@ const io = new Server(server, {
   },
 });
 
-// Set up logging with Winston
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
-    })
-  ),
-  transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({ filename: "server.log" }),
-  ],
-});
-
 // Default route to confirm the server is running
 app.get("/", (req, res) => {
   res.send("Socket.IO Server is running!");
@@ -44,44 +28,23 @@ app.get("/", (req, res) => {
 
 // Handle socket connection
 io.on("connection", (socket) => {
-  logger.info("A user connected");
+  console.log(`[${new Date().toISOString()}] A user connected`);
 
   // Listen for a message from the client
   socket.on("message", (msg) => {
-    logger.info(`Message received: ${msg}`);
+    console.log(`[${new Date().toISOString()}] Message received: ${msg}`);
     // Broadcast the message to all connected clients
     io.emit("message", msg);
   });
 
   // Handle user disconnection
   socket.on("disconnect", () => {
-    logger.info("A user disconnected");
+    console.log(`[${new Date().toISOString()}] A user disconnected`);
   });
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  logger.error(`Error occurred: ${err.message}`);
-  res.status(500).send("Something went wrong!");
 });
 
 // Start the server
 server.listen(PORT, () => {
-  logger.info(`Server is running on http://localhost:${PORT}`);
-  logger.info(`Frontend URL: ${FRONTEND_URL}`);
-});
-
-// Optional: Example for namespaces
-const chatNamespace = io.of("/chat");
-chatNamespace.on("connection", (socket) => {
-  logger.info("User connected to chat namespace");
-
-  socket.on("message", (msg) => {
-    logger.info(`Chat namespace message: ${msg}`);
-    chatNamespace.emit("message", msg);
-  });
-
-  socket.on("disconnect", () => {
-    logger.info("User disconnected from chat namespace");
-  });
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Frontend URL: ${FRONTEND_URL}`);
 });
