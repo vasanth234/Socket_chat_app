227b9053e77ed7f700ee4553b6d7757e2d7b5dff
vasanth234 <pipparivasanth66700@gmail.com>
2024-12-25 13:50:45 +0530

    updated the frontend url in backend

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:57264", // Angular app URL
+    origin: "https://socket-frontend-pink.vercel.app/", // Angular app URL
     methods: ["GET", "POST"],
   },
 });
